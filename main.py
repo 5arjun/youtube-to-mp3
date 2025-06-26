c1438fff159c8473a163325b4715f412d0090efc
Arjun Patel <tuo45748@temple.edu>
2025-06-26 17:08:58 -0400

    test

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, send_file, request
 import yt_dlp
 import tempfile
 import os
+port = int(os.environ.get("PORT", 5000))
 
 app = Flask(__name__)
+app.run(debug=False, host="0.0.0.0", port=port)
 
 @app.route("/")
 def index():
